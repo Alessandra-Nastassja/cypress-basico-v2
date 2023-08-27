efba82569490eb9e082f0a3b97057cc23fba6136
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-08-27 20:39:56 -0300

    feat: tests with selects

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -86,13 +86,21 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('.success').should('be.visible');
   });
 
-  it.only('seleciona um produto (YouTube) por seu texto', () => {
+  it('seleciona um produto (YouTube) por seu texto', () => {
     cy.get('#product')
       .select('YouTube')
       .should('have.value', 'youtube');
   });
 
-  it('seleciona um produto (Mentoria) por seu valor (value)', () => {})
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria');
+  });
 
-  it('seleciona um produto (Blog) por seu índice', () => {})
+  it.only('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog');
+  })
 })
