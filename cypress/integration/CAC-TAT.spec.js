59a0b6f5194bccc8adf6808fd3a64d50e61bbb2c
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-08-26 23:08:25 -0300

    fix: the best form to use type in button

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -3,7 +3,29 @@
 describe('Central de Atendimento ao Cliente TAT', function () {
   beforeEach(() => cy.visit('../src/index.html'));
 
-  it('verifica o título da aplicação', function () {
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
-  }) 
+  });
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Alessandra');
+    cy.get('#lastName').type('Santos');
+    cy.get('#email').type('alessandra@hotmail.com');
+    cy.get('#open-text-area').type('Lorem ipsum');
+    
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.success').should('be.visible');
+  });
+
+
+
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
+  xit('', () => {})
 })
