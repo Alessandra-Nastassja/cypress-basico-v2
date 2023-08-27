3064e32fb7e0b502520785e5ff5aeaa500954c88
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-08-26 23:26:23 -0300

    feat: verify long text and invalid email

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,19 +8,29 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   });
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = "Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem, Lorem";
+    
     cy.get('#firstName').type('Alessandra');
     cy.get('#lastName').type('Santos');
     cy.get('#email').type('alessandra@hotmail.com');
-    cy.get('#open-text-area').type('Lorem ipsum');
+    cy.get('#open-text-area').type(longText, { delay: 0 });
     
     cy.get('button[type="submit"]').click();
 
     cy.get('.success').should('be.visible');
   });
 
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#email').clear();
+    cy.get('#email').type('alessandra');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.error').should('be.visible');
+  });
 
 
-  xit('', () => {})
+
   xit('', () => {})
   xit('', () => {})
   xit('', () => {})
